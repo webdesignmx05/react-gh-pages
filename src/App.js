5eebcea401777cc699378aaef3dd9c75e0e0358f
webdesignmx05 <webdesignmx05@gmail.com>
2022-04-19 17:14:45 -0400

    Added another message to App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. You'll Thank me!
+          Edit <code>src/App.js</code> and save to reload. Fantastic!
         </p>
         <a
           className="App-link"
